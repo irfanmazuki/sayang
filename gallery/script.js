aa4389c82f1dab06636f16b4ee11c2d4c3b86863
Irfan <irfanmazuki9@gmail.com>
2025-02-19 17:34:47 +0800

    Update styling

--- gallery/script.js
+++ gallery/script.js
@@ -10,3 +10,28 @@ $(".polaroids li a").on("mouseenter", function () {
     }
   });
   
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const heartsContainer = document.getElementById('hearts-container');
+  
+    // Function to create falling hearts
+    function createHeart() {
+      const heart = document.createElement('div');
+      heart.classList.add('heart');
+      if(Math.random() < 0.5){
+        heart.classList.add("blue-heart");      
+      }
+      heart.style.left = `${Math.random() * 100}vw`;
+      heart.style.animationDuration = `${2 + Math.random() * 3}s`; // Random fall duration
+      heartsContainer.appendChild(heart);
+  
+      // Remove heart after animation
+      setTimeout(() => {
+        heart.remove();
+      }, 5000);
+    }
+
+    // Start falling hearts
+    setInterval(createHeart, 200);
+
+  });
